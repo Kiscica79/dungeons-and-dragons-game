f647ea96c445e96ec76a9e3c13c3073f007e31a0
Kiscica79 <oroszildiko79@gmail.hu>
2023-06-10 17:51:42 +0200

    Még nincs kész

--- src/Game.java
+++ src/Game.java
@@ -1,44 +1,78 @@
+import model.Character;
 import model.Monster;
 
+import java.util.Scanner;
+
 public class Game {
+
+
+
     public static void main(String[] args) {
+
         // Kérd meg a felhasználót, hogy adja meg a karaktere nevét és ezt olvasd be egy változóba!
         // (Használj Scannert!)
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Add meg a karaktered nevét ");
+        String user = scanner.nextLine();
 
         // Példányosíts egy Charactert (hívd meg a konstruktorát) ezzel a változóval!
+        Character player  = new Character(user);
 
-        // Írd ki: Welcome, [játékos neve]!
 
         // Készíts egy 3 hosszúságú tömböt Monster-ekből!
+        Monster[] monster = new Monster[3];
 
         // Töltsd fel szörnyekkel! Példa: monsters[0] = new Monster("Goblin", 50);
+        monster[0] = new Monster("Goblin", 50);
+        monster[1] = new Monster("Troll", 23);
+        monster[2] = new Monster("Dragon", 89);
 
         // Készíts egy gameOver nevű logikai változót, állítds az értékét false-ra!
+        boolean gameover = false;
 
         // Írj egy ciklust, ami addig tart amíg nincs vége a játéknek (amíg a gameOver nem true) ÉS
         // amíg a karakter élete 0-nál nagyobb!
+        while (gameover != true && player.getHealth() > 0) {
 
-            // Írd ki: Choose a monster to fight or quit:
+        }
 
-            // Írd ki: 0: Quit
+        // Írd ki: Choose a monster to fight or quit:
+        System.out.println("Choose a monster to fight or quit: ");
 
-            // Írd ki a Monstereket tartalmazó tömb összes elemének a nevét ebben a formátumban:
-            // 1: Goblin
-            // 2: Troll
-            // 3: Dragon
+        // Írd ki: 0: Quit
+        System.out.println("0: Quit");
 
-            // Olvasd be egy choice nevű változóba a felhasználótól, hogy mit választ: kilép vagy valamelyik
-            // szörnnyel harcol!
+        // Írd ki a Monstereket tartalmazó tömb összes elemének a nevét ebben a formátumban:
+        // 1: Goblin
+        // 2: Troll
+        // 3: Dragon
+        for (int i = 0; i < monster.length; i++) {
+            System.out.println(i+1+": "+monster[i].getName());
+        }
 
-            // Ha azt választja, hogy kilép (0), írd ki: Thanks for playing! Goodbye.
-            // És állítsd be a gameOver változó értékét true-ra!
+        // Olvasd be egy choice nevű változóba a felhasználótól, hogy mit választ: kilép vagy valamelyik
+        // szörnnyel harcol!
+        System.out.println("Mit választasz? Kilépsz(0) vagy Játszol (1,2 vagy 3)?");
+        int choice = scanner.nextInt();
 
-            // Ha jó inputot ad meg (1 és tömb hossza között) és harcol:
-            // Tárold el egy Monster típusú változóban, hogy melyiket választotta a felhasználó!
+        // Ha azt választja, hogy kilép (0), írd ki: Thanks for playing! Goodbye.
+        // És állítsd be a gameOver változó értékét true-ra!
+        if (choice == 0) {
+            System.out.println("Thanks for playing! Goodbye.");
+            gameover = true;
+        }
 
-            // Írj egy ciklust, ami addig megy, amíg a szörny és a játékos élete is nagyobb mint 0!
-                // Támadja meg a felhasználó karaktere a szörnyet!
-                // Ha a szörny élete továbbra is nagyobb, mint 0, akkor a szörny támadjon vissza!
+        // Ha jó inputot ad meg (1 és tömb hossza között) és harcol:
+        // Tárold el egy Monster típusú változóban, hogy melyiket választotta a felhasználó!
+        if (choice == 1 || choice == 2 || choice == 3) {
+
+        }
+
+        // Írj egy ciklust, ami addig megy, amíg a szörny és a játékos élete is nagyobb mint 0!
+
+        // Támadja meg a felhasználó karaktere a szörnyet!
+
+        // Ha a szörny élete továbbra is nagyobb, mint 0, akkor a szörny támadjon vissza!
 
         // Ha rossz inputot ad meg, írd ki: Invalid choice!
 
--- src/model/Monster.java
+++ src/model/Monster.java
@@ -1,19 +1,46 @@
 package model;
 
 public class Monster {
+    private String name;
+    private int health;
 
-    // Vegyél fel két fieldet: name (String) és health (int)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
 
     // Írj egy két paraméteres kosntruktort ami beállítja az attribútumok értékeit!
+    public Monster(String name, int health) {
+        this.name = name;
+        this.health = health;
+    }
 
     public void attack(Character player) {
+
         // Írd ki: [szörny neve] attacks [játékos neve]!
+        System.out.println(name + "attack" + player.getName());
 
         // Csökkentsd a játékos életét 20-szal!
+        player.setHealth(getHealth() - 20);
 
         // Írd ki: Dealt 20 damage to [játékos neve]!
+        System.out.println("Dealt 20 damage to " + name);
 
         // Ha a játékos élete 0 vagy kisebb, írd ki: [játékos neve] has been defeated!
-
+        if (player.getHealth() < 0) {
+            System.out.println(player.getName() + " has been defeated!");
+        }
     }
+
 }
